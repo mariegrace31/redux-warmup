36df3999aa7c3f6be25031c32dedcb9d3f7b6047
mariegrace31 <mariegracebmg@gmail.com>
2023-08-01 13:33:43 +0200

    Add the features folder

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// import the store
+import { store } from './store';
+import { Provider } from 'react-redux';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
