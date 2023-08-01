6553a47acf838d438ff7105368dbcdea70f19564
mariegrace31 <mariegracebmg@gmail.com>
2023-08-01 17:02:29 +0200

    Add the remove function

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { ChevronDown, ChevronUp } from '../icons';
+import { removeItem } from '../features/cart/cartSlice';
+import {useDispatch} from 'react-redux';
 
 const CartItem = ({ id, img, title, price, amount}) => {
+  const dispatch = useDispatch();
   return (
    <article className='cart-item'>
     <img src={img} alt={title} />
     <div>
       <h4>{title}</h4>
       <h4 className='item-price'>${price}</h4>
-      <button className='remove-btn'>Remove</button>
+      <button className='remove-btn' onClick={() => {
+        dispatch(removeItem(id));
+      }}>
+        Remove</button>
     </div>
     <div>
       <button className='amount-btn'>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -15,9 +15,14 @@ const cartSlice = createSlice({
     clearCart:(state) => {
       state.cartItems = [];
     },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => 
+      item.id !== itemId);
+    }
   },
 });
 
 // console.log(cartSlice);
-export const{clearCart} = cartSlice.actions;
+export const{clearCart, removeItem} = cartSlice.actions;
 export default cartSlice.reducer;
