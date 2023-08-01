7bb8f3ec25e818dfa518c06c875ac051c35a47ba
mariegrace31 <mariegracebmg@gmail.com>
2023-08-01 20:07:19 +0200

    Add the close modal function

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import CartItem from './CartItem';
 import {useDispatch,useSelector} from 'react-redux';
-import { clearCart } from '../features/cart/cartSlice';
+import { openModal } from '../features/modal/modalSlice';
 
 const CartContainer = () => {
   const dispatch = useDispatch();
-  const {CartItems,total,amount} = useSelector((store) => store.cart);
+  const {cartItems,total,amount} = useSelector((store) => store.cart);
   if (amount < 1) {
     return (
       <section className='cart'>
@@ -22,7 +21,7 @@ const CartContainer = () => {
       <h2>Your bag</h2>
     </header>
     <div>
-      {CartItems.map((item) => {
+      {cartItems.map((item) => {
         return <CartItem key = {item.id} {...item} />;
       })}
     </div>
@@ -32,7 +31,7 @@ const CartContainer = () => {
         <h4>total <span>${total.toFixed(2)}</span></h4>
       </div>
       <button className='btn clear-btn'
-      onClick={() => dispatch(clearCart())}>Clear cart
+      onClick={() => dispatch(openModal())}>Clear cart
       </button>
     </footer>
    </section>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,26 @@
+import {useDispatch} from 'react-redux';
+import { clearCart } from '../features/cart/cartSlice';
+import { closeModal } from '../features/modal/modalSlice';
+
 const Modal = () => {
+  const dispatch = useDispatch();
   return (
     <aside className="modal-container">
       <div className="modal">
         <h4>Remove all items from your shopping cart?</h4>
         <div className="btn-container">
-          <button type="button" className="btn confirm-btn">Confirm</button>
-          <button type="button" className="btn clear-btn">Cancel</button>
+          <button type="button" className="btn confirm-btn" 
+          onClick={() => {
+            dispatch(clearCart());
+            dispatch(closeModal());
+          }}
+          >
+            Confirm</button>
+          <button type="button" className="btn clear-btn"
+             onClick={() => {
+              dispatch(closeModal());
+            }}
+          >Cancel</button>
         </div>
       </div>
     </aside>
