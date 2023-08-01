7832347d8bfbc4acb2f9fb6bfee022c5deb27c2d
mariegrace31 <mariegracebmg@gmail.com>
2023-08-01 19:22:58 +0200

    'Update the modal

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import CartContainer from "./components/CartContainer";
 import {useDispatch, useSelector} from 'react-redux';
 import { calculateTotals } from "./features/cart/cartSlice";
 import { useEffect } from "react";
+import Modal from "./components/Modal";
 
 function App() {
   const {cartItems} = useSelector((store) => store.cart);
+  const {isOpen} = useSelector((store) => store.modal);
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
 
   return (
     <main>
-      <Modal />
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
